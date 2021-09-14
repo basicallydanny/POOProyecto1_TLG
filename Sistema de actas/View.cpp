ed13f29ffb6a8e3e33d7d83e7cfde50b94364faa
jmlopezriani <jmlopezriani@javerianacali.edu.co>
2021-09-13 20:06:11 -0500

    Se introdujo funcionalidad para guardar estado de sistema de actas en archivo txt

--- Sistema de actas/View.cpp
+++ Sistema de actas/View.cpp
@@ -4,6 +4,8 @@
 
 void View::mostrarMenuGeneral(){
     int opc = -1;
+    fstream file_obj;
+
     do {
         cout << "1. Cargar Sistema \n";
         cout << "2. Nuevo Sistema \n";
@@ -13,10 +15,21 @@ void View::mostrarMenuGeneral(){
 
         switch (opc){
         case 1:
-            // TODO: guardar actas y personal
+            file_obj.open("sistemaActas.txt", ios::out);
+            file_obj.read((char*)&sistema, sizeof(sistema));
+            file_obj.close();
+            //introducir metodos menu
+            file_obj.open("sistemaActas.txt", ios::trunc);
+            file_obj.write((char*)&sistema, sizeof(sistema));
+            file_obj.close();
+            
         break;
         case 2:
+            file_obj.open("sistemaActas.txt", ios::trunc);
+            //introducir metodos menu
             sistema.mostrarMenuAsistente();
+            file_obj.write((char*)&sistema, sizeof(sistema));
+            file_obj.close();
         break;
         }
     } 
--- Sistema de actas/View.h
+++ Sistema de actas/View.h
@@ -2,10 +2,13 @@
 #ifndef VIEW_H
 #define VIEW_H
 //Incluir librerias 
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include "DireccionPos.h"
+#include <fstream>
 
+using namespace std;
 using std::cout;
 using std::cin;
 using std::string;
