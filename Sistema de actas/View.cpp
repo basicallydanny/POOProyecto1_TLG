e19db2b670880a2f5741e815f4527a42eb3e1efb
jmlopezriani <jmlopezriani@javerianacali.edu.co>
2021-09-14 21:56:18 -0500

    iniciacion con usuarios

--- Sistema de actas/View.cpp
+++ Sistema de actas/View.cpp
@@ -19,15 +19,24 @@ void View::mostrarMenuGeneral(){
             leer.seekg(0);
             leer.read((char*)&sistema, sizeof(sistema));
             leer.close();
+            
+            cout<<"Creacion de usuario jurado:\n";
             sistema.insertUsuario(sistema.nuevoUsuario());
+            cout<<"Creacion de usuario asistente:\n";
+            sistema.insertUsuario(sistema.nuevoUsuario());
+            cout<<"Creacion de usuario director:\n";
+            sistema.insertUsuario(sistema.nuevoUsuario());
+            cout<<"Ingrese el nombre del usuario que ingresara: \n";
+            cin>>usuarioA;
             sistema.loginUsuario(usuarioA);
+
             escribir.open("sistemaActas.txt", ios::trunc);
             escribir.clear();
             escribir.write((char*)&sistema, sizeof(sistema));
             escribir.close();
         break;
         case 2:
-            cout<<"Ingrese el usuario: \n";
+            cout<<"Ingrese el nombre del usuario que ingresara: \n";
             cin>>usuarioA;
             sistema.loginUsuario(usuarioA);
             escribir.open("sistemaActas.txt", ios::trunc);
